697e425fac9dcb34cb4703ff3750964acaa97a47
WKMeng <wakenmeng@gmail.com>
2014-04-17 22:20:12 +0800

    some changes
    show path
    change view
    cmd: cd /, cd .. /, cd /root/mydir .....

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 Simple Shell
 ===============================
 unix shell and file system
-###
+### 文件类型定义
 
-cammand: ls cd mkdir
 type file:1 direcotry: 0
 
 ### 文件存储结构
 
 id  name    lvl    type    pid
+0	/		0		0		-1
+1   root    1       0       0
 
-1   root    0       0       -1
+### 指令
+
+cd: 绝对路径
+	子文件夹
+	cd .. 回到上级目录
+	cd /  回到文件根目录
+	判错
+ls: 显示当前目录内文件
+mkdir：创建文件夹
+		同名文件夹判错
--- file.php
+++ file.php
@@ -44,12 +44,18 @@ function fun_ls($pid) {
 }
 function fun_cd($data){
     $pid = $data['id'];
-    $arg = $data['arg'];
+    $arg = explode('/',$data['arg']);
+    if($arg[0]=='') 
+    {   //绝对路径
+        $pid = '-1';
+        $arg[0]='/';
+    }
     $res = array();
+    $final=array();
     $fd = fopen('map.txt', 'r');
     $read = fread($fd,filesize('map.txt'));
     $line = explode("\n",$read);
-    if($arg==".."){
+    if(count($arg)==1 && $arg[0]==".."){
         if($data['pid']==-1){
             fclose($fd);
             return $res;
@@ -65,38 +71,65 @@ function fun_cd($data){
                 $res['lvl']=$re[2];
                 $res['type']=$re[3];
                 $res['pid']=$re[4];
+                array_push($final,$res);
+                break;
             }
         }
     }
     else{
-        foreach($line as $l)
-        {
-            $re = explode(' ',$l);
-            if((int)$re[4]==(int)$pid && $re[1]==$arg)
+        foreach ($arg as $dir) {
+            foreach($line as $l)
             {
-                if((int)$re[3]==1)
-                    $res['er']=":Not a directory";
-                else{
-                    $res['id']=$re[0];
-                    $res['name']=$re[1];
-                    $res['lvl']=$re[2];
-                    $res['type']=$re[3];
-                    $res['pid']=$re[4];
+                $re = explode(' ',$l);
+                if((int)$re[4]==(int)$pid && $re[1]==$dir)
+                {
+                    if((int)$re[3]==1){
+                        fclose($fd);
+                        $res['er']=":Not a directory";
+                        return $res;
+                    }
+                    else{
+                        $res['id']=$re[0];
+                        $res['name']=$re[1];
+                        $res['lvl']=$re[2];
+                        $res['type']=$re[3];
+                        $res['pid']=$re[4];
+                        $pid=$res['id'];
+                        array_push($final,$res);
+                        break;
+                    }
                 }
             }
+        if(empty($final)){
+            fclose($fd);
+            $res['empty']=$dir;
+           $res['er']=$dir.":No such file or directory";
+            return $res;
+            }
+        $res=array();
         }
-        if(empty($res))
-            $res['er']=":No such file or directory";
     }
     fclose($fd);
-    return $res;
+    return $final;
 }
 function fun_mkdir($data)
 {
-    $id=$data['id'];
+    $pid=$data['id'];
     $arg=$data['arg'];
-    $line=count(file('map.txt'));
-    $string = "\n".$line." ".$arg." ".'0'." ".'0'." ".$id;
+    //判重
+    $fd=fopen('map.txt','r');
+    $read = fread($fd, filesize('map.txt'));
+    $line = explode("\n",$read);
+    foreach ($line as $l) {
+       $re=explode(' ', $l);
+       if((int)$pid==(int)$re[4] && $arg==$re[1] && (int)$re[3]==0)
+       {
+        $res['er']="mkdir: cannot create directory\'".$arg."\': File exists";
+        return $res;
+       } 
+    }
+    $lvl=(int)$data['lvl']+1;
+    $string = "\n".count($line)." ".$arg." ".$lvl." ".'0'." ".$pid;
     file_put_contents('map.txt',$string,FILE_APPEND);
     return '';
 }
